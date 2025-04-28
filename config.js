8185560f55cd7ac2cb6158604de7e5de36d9121e
Queen-anita <queenanita1122@gmail.com>
2025-04-28 15:06:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PkdrillerFL1kxAZB#1Ko4v3YZ1RpuXa75aLyQGwWdO4MwPwTiS4fDMFsWSPU",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
